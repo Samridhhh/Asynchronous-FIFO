15a155c28e33e3b91447c31898282b61ef324cef
Samridh Shukla <smshukla2020@gmail.com>
2025-07-16 16:32:36 +0530

    Update rptr_handler.v

--- Project_A/Project_A.srcs/sources_1/new/rptr_handler.v
+++ Project_A/Project_A.srcs/sources_1/new/rptr_handler.v
@@ -4,7 +4,7 @@ module rptr_handler #(
 )(
     input  wire                 rclk,
     input  wire                 rst_n,
-    input  wire                 r_en, // rclk is ticking and enable =0
+    input  wire                 r_en,
     input  wire [PTR_WIDTH:0]   g_wptr_sync,
     output reg  [PTR_WIDTH:0]   b_rptr,
     output reg  [PTR_WIDTH:0]   g_rptr,
@@ -17,7 +17,7 @@ module rptr_handler #(
   assign b_rptr_next = b_rptr + (r_en & ~empty);
   assign g_rptr_next = (b_rptr_next >> 1) ^ b_rptr_next;
 
-  // Update pointers
+ 
   always @(posedge rclk or negedge rst_n) begin
     if (!rst_n) begin
       b_rptr <= {PTR_WIDTH+1{1'b0}};
@@ -28,7 +28,7 @@ module rptr_handler #(
     end
   end
 
-  // Empty flag logic
+  
   always @(posedge rclk or negedge rst_n) begin
     if (!rst_n)
       empty <= 1'b1;
